766744b556776624a03ea53463eb8ce5bace98c7
ViniFarias <vinifarias.vf@gmail.com>
2019-10-02 17:34:30 -0300

    chore: add tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,81 @@
+import gql from "graphql-tag";
+import ApolloClient from "apollo-boost";
+import fetch from "node-fetch";
 import { expect } from "chai";
+import { TestxServer } from "../src/index";
 
-describe("", () => {
-  it("should be true", () => {
-    expect(true).to.be.true;
+describe("test mutations", () => {
+  let server;
+  let client;
+
+  const CREATE_ITEM = gql`
+    mutation createItem($title: String!) {
+      createItem(input: { title: $title }) {
+        id
+        title
+      }
+    }
+  `;
+
+  const UPDATE_ITEM = gql`
+    mutation updateItem($id: ID!, $title: String!) {
+      updateItem(id: $id, input: { title: $title }) {
+        id
+        title
+      }
+    }
+  `;
+
+  const FIND_ALL_ITEMS = gql`
+    query findAllItems {
+      findAllItems {
+        id
+        title
+      }
+    }
+  `;
+
+  before("start graphql server", async () => {
+    server = new TestxServer(`
+      type Item {
+        id: ID!
+        name: String
+        title: String!
+      }`
+    );
+    await server.start();
+    console.log(`Running on ${server.url()}`)
+  });
+
+  after("close graphql server", () => {
+    server.close();
+  });
+
+  before("initialize apollo client", () => {
+    client = new ApolloClient({ uri: server.url(), fetch: fetch });
+  });
+
+  it("should create a new item", async () => {
+    const item = (await client.mutate({
+      mutation: CREATE_ITEM,
+      variables: { title: "TestA" }
+    })).data.createItem;
+
+    expect(item.id).to.not.be.null;
+    expect(item.title).to.be.equal("TestA");
+  });
+
+  it("should update existing item", async () => {
+    const testA = (await client.query({
+      query: FIND_ALL_ITEMS
+    })).data.findAllItems.find(item => item.title === "TestA");
+
+    let testB = (await client.mutate({
+      mutation: UPDATE_ITEM,
+      variables: { id: testA.id, title: "TestB" }
+    })).data.updateItem;
+
+    expect(testB.id).to.be.equal(testA.id);
+    expect(testB.title).to.be.equal("TestB");
   });
 });
