5e9423bd23eff9f677aba3d972e01969c1369fa7
ViniFarias <vinifarias.vf@gmail.com>
2019-10-09 17:00:55 -0300

    feat: connect with database

--- src/graphback-core/index.ts
+++ src/graphback-core/index.ts
@@ -1,8 +1,13 @@
-import { GraphQLBackendCreator, DatabaseSchemaManager } from 'graphback';
+import express from "express";
+import { DatabaseSchemaManager, GraphQLBackendCreator } from "graphback";
+import knex from "knex";
 import { transpile } from "typescript";
-import * as folders from './config.json';
+import * as folders from "./config.json";
 
-export async function generateTypeDefs(model: string, config: {[id: string]: any}) {
+export async function generateTypeDefs(
+  model: string,
+  config: { [id: string]: any },
+) {
   const creator = new GraphQLBackendCreator(model, config);
   const backend = await creator.createBackend("sqlite3");
 
@@ -10,16 +15,20 @@ export async function generateTypeDefs(model: string, config: {[id: string]: any
   return typeDefs;
 }
 
-export async function generateResolvers(model: string, config: {[id: string]: any}) {
+export async function generateResolvers(
+  model: string,
+  config: { [id: string]: any },
+) {
   const creator = new GraphQLBackendCreator(model, config);
   const backend = await creator.createBackend("sqlite3");
-
   const modules: { [id: string]: any } = {};
+
   for (const resolver of backend.resolvers.types) {
     modules[`./generated/${resolver.name}`] = sourceModule(
       transpile(resolver.output),
     );
   }
+
   const { resolvers } = sourceModule(
     transpile(backend.resolvers.index),
     modules,
@@ -28,30 +37,54 @@ export async function generateResolvers(model: string, config: {[id: string]: an
   return resolvers;
 }
 
-export async function generateDatabase(schemaText, config) {
-  const backend: GraphQLBackendCreator  = new GraphQLBackendCreator(schemaText, config);
+export async function generateDatabase(
+  model: string,
+  config: { [id: string]: any },
+) {
+  const backend: GraphQLBackendCreator = new GraphQLBackendCreator(
+    model,
+    config,
+  );
 
   const connectionConfig = {
-    filename: `${folders.database}/db.sqlite`
+    filename: `${folders.database}/db.sqlite`,
   };
 
-  const manager = new DatabaseSchemaManager('sqlite3', connectionConfig);
+  const manager = new DatabaseSchemaManager("sqlite3", connectionConfig);
   backend.registerDataResourcesManager(manager);
 
   await backend.createDatabase();
+}
+
+export async function generateContext() {
+  const client = knex({
+    client: "sqlite3",
+    connection: { filename: `${folders.database}/db.sqlite` },
+    useNullAsDefault: true,
+  });
+
+  const context = async ({ req }: { req: express.Request }) => {
+    return {
+      req,
+      db: client,
+    };
+  };
+
+  return context;
 }
 
 function sourceModule(input: string, modules: { [id: string]: any } = {}): any {
   const m = new Function("exports", "require", "module", input);
   const fakeExports = { default: null };
   const fakeModule = { exports: fakeExports };
+  
   const fakeRequire = (id: string) => {
     if (id in modules) {
       return modules[id];
     }
     return require(id);
   };
-  
+
   m(fakeExports, fakeRequire, fakeModule);
   return fakeExports;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,55 @@
 import { ApolloServer } from "apollo-server";
-import { generateTypeDefs, generateResolvers, generateDatabase } from "./graphback-core/index";
-import { Server } from 'http';
+import { Server } from "http";
+import {
+  generateContext,
+  generateDatabase,
+  generateResolvers,
+  generateTypeDefs,
+} from "./utils/index";
 
 const defaultConfig = {
-    "create": true,
-    "update": true,
-    "findAll": true,
-    "find": true,
-    "delete": true,
-    "subCreate": true,
-    "subUpdate": true,
-    "subDelete": true,
-    "disableGen": false
-  }
+  create: true,
+  update: true,
+  findAll: true,
+  find: true,
+  delete: true,
+  subCreate: false,
+  subUpdate: false,
+  subDelete: false,
+  disableGen: false,
+};
 
 export class TestxServer {
-    private schema: string;
-    private server: Server;
-    private serverUrl: string;
+  private schema: string;
+  private server: Server;
+  private serverUrl: string;
 
-    constructor(schema: string) {
-        this.schema = schema;
-    }
+  constructor(schema: string) {
+    this.schema = schema;
+  }
 
-    public async start() {
-        const apolloServer = await this.generateServer();
-        await generateDatabase(this.schema, defaultConfig);
+  public async start() {
+    const apolloServer = await this.generateServer();
+    await generateDatabase(this.schema, defaultConfig);
 
-        const { server, url } = await apolloServer.listen();
-        this.server = server;
-        this.serverUrl = url;
-    }
+    const { server, url } = await apolloServer.listen();
+    this.server = server;
+    this.serverUrl = url;
+  }
 
-    public close() {
-        this.server.close();
-    }
+  public close() {
+    this.server.close();
+  }
 
-    public url() {
-        return this.serverUrl;
-    }
+  public url() {
+    return this.serverUrl;
+  }
 
-    private async generateServer() {
-        const typeDefs = await generateTypeDefs(this.schema, defaultConfig);
-        const resolvers = await generateResolvers(this.schema, defaultConfig);
-
-        const apolloServer = new ApolloServer({ typeDefs, resolvers });
-        return apolloServer;
-    }
+  private async generateServer() {
+    const typeDefs = await generateTypeDefs(this.schema, defaultConfig);
+    const resolvers = await generateResolvers(this.schema, defaultConfig);
+    const context = await generateContext();
+    const apolloServer = new ApolloServer({ typeDefs, resolvers, context });
+    return apolloServer;
+  }
 }
