414abb853c470eadb8072c81cc65a6b288b3efab
Davide <david3bizzarri@gmail.com>
2019-11-18 19:49:00 +0100

    chore: add parallel  start

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,13 @@ import test from "ava";
 import { request } from "graphql-request";
 import { TestxServer } from ".";
 
+const ITEM_MODEL = `
+  type Item {
+    id: ID!
+    title: String!
+  }
+`;
+
 test("start TestxServer server", async t => {
   const FIND_ALL_ITEMS = `
     query findAllItems {
@@ -12,16 +19,22 @@ test("start TestxServer server", async t => {
     }
   `;
 
-  const server = new TestxServer(`
-    type Item {
-      id: ID!
-      title: String!
-    }
-  `);
+  const server = new TestxServer(ITEM_MODEL);
 
   await server.start();
   t.assert(server.url());
 
   const result = await request(server.url(), FIND_ALL_ITEMS);
   t.assert(result.findAllItems.length === 0);
+});
+
+test.skip("start multiple TestxServer servers at the same time", async t => {
+  // Issue: https://github.com/aerogear/graphql-testx/issues/47
+  const server1 = new TestxServer(ITEM_MODEL);
+  const server2 = new TestxServer(ITEM_MODEL);
+
+  // start both servers at the same time
+  await Promise.all([server1.start(), server2.start()]);
+
+  t.assert(true);
 });
