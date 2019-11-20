afbac20be7a9ff98cf7df701ebeed9204768428e
ViniFarias <vinifarias.vf@gmail.com>
2019-11-20 15:23:26 -0300

    test: update apollo-client example

--- examples/apollo-client/test/mutations.spec.ts
+++ examples/apollo-client/test/mutations.spec.ts
@@ -19,24 +19,6 @@ describe("test mutations", () => {
     }
   `;
 
-  const UPDATE_ITEM = gql`
-    mutation updateItem($id: ID!, $title: String!) {
-      updateItem(id: $id, input: { title: $title }) {
-        id
-        title
-      }
-    }
-  `;
-
-  const FIND_ALL_ITEMS = gql`
-    query findAllItems {
-      findAllItems {
-        id
-        title
-      }
-    }
-  `;
-
   before("start graphql server", async () => {
     const schema = readFileSync(resolve(__dirname, '../fixtures/schema.graphql'), 'utf8');
     server = new TestxServer(schema);
@@ -66,13 +48,13 @@ describe("test mutations", () => {
 
   it("should update existing item", async () => {
     const findResult = await client.query({
-      query: FIND_ALL_ITEMS
+      query: gql`${server.getQueries().findAllItems}`
     });
     const itemV1 = findResult.data.findAllItems.find(i => i.title === "TestA");
     expect(itemV1).to.be.exist;
 
     const updateResult = await client.mutate({
-      mutation: UPDATE_ITEM,
+      mutation: gql`${server.getMutations().updateItem}`,
       variables: { id: itemV1.id, title: "TestB" }
     });
     const itemV2 = updateResult.data.updateItem;
