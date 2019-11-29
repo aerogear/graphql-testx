5a6a473a0683a3a15810a8550641b5655bcc08c9
Davide <david3bizzarri@gmail.com>
2019-11-29 14:38:13 +0100

    chore: export TestxController and TestxDirector

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export { TestxServer } from "./TestxServer"
+export { TestxServer } from "./TestxServer";
+export { TestxController } from "./TestxController";
+export { TestxDirector } from "./TestxDirector";
