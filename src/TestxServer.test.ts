0fd9c46270acc170ce50da5d38c457ad4061ed97
ViniFarias <vinifarias.vf@gmail.com>
2019-12-02 14:17:27 -0300

    test: fix TestxServer test

--- src/TestxServer.test.ts
+++ src/TestxServer.test.ts
@@ -33,7 +33,6 @@ test("test start() and close() methods", async t => {
 
 test.only("should start the server after closing it", async t => {
   const server = new TestxServer(ITEM_MODEL);
-
   await server.start();
   const httpUrl = await server.httpUrl()
   const mutations = await server.getMutations();
